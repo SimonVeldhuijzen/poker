c8ed07682f93f0cea66ce9907c9321a884bb4c84
Joeri Landman <joeri.landman@nedap.com>
2020-01-24 18:13:16 +0100

    yolo

--- src/main/kotlin/poker/players/PokermonMaster.kt
+++ src/main/kotlin/poker/players/PokermonMaster.kt
@@ -1,11 +1,16 @@
 package poker.players
 
 import poker.*
+import java.lang.Double.max
+import kotlin.math.abs
+import kotlin.math.max
 
 class PokermonMaster(var safeLevel: Int = 1, var jeMoederLevel: Int = 4): AIPlayer {
     override var name = "PokermonMaster"
 
     override fun move(state: Board, player: Player): PlayerAction {
+        val round = state.communityCards.size
+        var maxBet = 100
         val handToRank = player.cards
         handToRank.addAll(state.communityCards)
         val rankedHand = rankHand(handToRank)
@@ -15,15 +20,24 @@ class PokermonMaster(var safeLevel: Int = 1, var jeMoederLevel: Int = 4): AIPlay
             } else {
                 return Raise(player, player.wealth * 999999999)
             }
+        } else if (round < 2) {
+            if (player.cards[0].suit == player.cards[1].suit) {
+                maxBet = max(1000, player.wealth / 10)
+            } else if (player.cards[0].rank.rank > 11 || player.cards[1].rank.rank > 11){
+                maxBet = max(1000, player.wealth / 10)
+            } else if (player.cards[0].rank.rank == player.cards[1].rank.rank ){
+                maxBet = max(2000, player.wealth / 5)
+            } else if (abs(player.cards[0].rank.rank - player.cards[1].rank.rank) == 1 ){
+                maxBet = max(500, player.wealth / 20)
+            }
+        }
+        if (player.betThisRound <= maxBet) {
+            return Call(player)
         } else {
-            if (player.betThisRound <= 100) {
+            if (state.currentBet - player.betThisRound <= maxBet) {
                 return Call(player)
             } else {
-                if (state.currentBet - player.betThisRound == state.minBet) {
-                    return Call(player)
-                } else {
-                    return Check(player)
-                }
+                return Check(player)
             }
         }
     }
