3c9943d7c031a1ec8449bb6030b5b723b4fcf5a6
Mitchell Vrugteveen <mail@mtchll.com>
2020-01-24 16:00:25 +0100

    MITCHELLBOT

--- src/main/kotlin/poker/players/mitchells/Mitchell.kt
+++ src/main/kotlin/poker/players/mitchells/Mitchell.kt
@@ -13,10 +13,16 @@ class Mitchell: AIPlayer {
         val pokerhand = rankHand(listOfCards)
 
         val playerAction: PlayerAction
-        if (pokerhand is HighCard || pokerhand is poker.Pair || pokerhand is TwoPair) {
-            playerAction = Check(player)
-        } else {
-            playerAction = listOfPlayers.random().move(state, player)
+        when (pokerhand) {
+            is HighCard -> playerAction = Check(player)
+            is Pair -> playerAction = Check(player)
+            is TwoPair -> playerAction = MitCall().move(state, player)
+            is ThreeOfAKind -> playerAction = Raisechell().move(state, player)
+            is Straight -> playerAction = Raisechell().move(state, player)
+            is Flush -> playerAction = Raisechell().move(state, player)
+            is FullHouse -> playerAction = Raisechell().move(state, player)
+            is FourOfAKind -> playerAction = MitchAllIn().move(state, player)
+            is StraightFlush -> playerAction = MitchAllIn().move(state, player)
         }
         return playerAction
     }
--- src/main/kotlin/poker/players/mitchells/Raisechell.kt
+++ src/main/kotlin/poker/players/mitchells/Raisechell.kt
@@ -2,12 +2,13 @@ package poker.players.mitchells
 
 import poker.*
 import java.util.*
+import kotlin.random.Random
 
 class Raisechell: AIPlayer {
 
     override var name: String = "Raisechell"
 
     override fun move(state: Board, player: Player): PlayerAction {
-        return Call(player)
+        return Raise(player, Random.nextInt(1, player.wealth))
     }
 }
