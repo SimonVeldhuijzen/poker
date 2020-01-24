d04cd160c1160b8acee525d35d4199e9a61d6941
Ramon Onis <ramon.onis@nedap.com>
2020-01-24 11:29:51 +0100

    science!

--- src/main/kotlin/poker/App.kt
+++ src/main/kotlin/poker/App.kt
@@ -2,9 +2,10 @@ package poker
 
 import poker.players.HumanPlayer
 import poker.players.RandomPlayer
+import poker.players.alsucfnhaiegsdruifjklgjdkljhjhgfdsdhjkjhgfdryuiyrwqyuikdsjkdmnbvcxvbnmfdgjkjretyutrewhjmnxvbnmnbcdfghjkjgweyuiutrewrj
 
 fun main(args: Array<String>) {
-    val players = listOf(RandomPlayer(), RandomPlayer(), RandomPlayer(), RandomPlayer()).mapIndexed { i, ai ->
+    val players = listOf(RandomPlayer(), HumanPlayer(), alsucfnhaiegsdruifjklgjdkljhjhgfdsdhjkjhgfdryuiyrwqyuikdsjkdmnbvcxvbnmfdgjkjretyutrewhjmnxvbnmnbcdfghjkjgweyuiutrewrj()).mapIndexed { i, ai ->
         Player(i, 10000, 0, 0, null, mutableListOf(), ai)
     }
 
--- src/main/kotlin/poker/players/alsucfnhaiegsdruifjklgjdkljhjhgfdsdhjkjhgfdryuiyrwqyuikdsjkdmnbvcxvbnmfdgjkjretyutrewhjmnxvbnmnbcdfghjkjgweyuiutrewrj.kt
+++ src/main/kotlin/poker/players/alsucfnhaiegsdruifjklgjdkljhjhgfdsdhjkjhgfdryuiyrwqyuikdsjkdmnbvcxvbnmfdgjkjretyutrewhjmnxvbnmnbcdfghjkjgweyuiutrewrj.kt
@@ -1,12 +1,43 @@
 package poker.players
 
-import poker.AIPlayer
-import poker.Board
-import poker.Player
-import poker.PlayerAction
+import poker.*
 
 class alsucfnhaiegsdruifjklgjdkljhjhgfdsdhjkjhgfdryuiyrwqyuikdsjkdmnbvcxvbnmfdgjkjretyutrewhjmnxvbnmnbcdfghjkjgweyuiutrewrj : AIPlayer {
     override fun move(state: Board, player: Player): PlayerAction {
-    }
+        val myBet = player.betThisRound
+        val requiredBet = state.currentBet
+        val myMunnie = player.wealth
+        val toCall = requiredBet - myBet
+        val dikkePot = state.players.sumBy(Player::betTotal)
+        val myCards = player.cards
+        val communityCards = state.communityCards
+        val deckCards = (CardSuit.values().flatMap { suit -> (2..14).map { rank -> Card(suit, CardRank(rank)) } } - communityCards - myCards).toMutableList()
+        val pCount = state.players.count(Player::isActive)
 
+        val start = System.currentTimeMillis()
+        var total = 0
+        var wins = 0.0
+        while (System.currentTimeMillis() - start < 950) {
+            deckCards.shuffle()
+            val lolRandom = deckCards.take(5 - communityCards.size + 2)
+            val other = communityCards + lolRandom.take(2)
+            val coms = lolRandom.drop(2)
+            val myHand = rankHand(myCards + coms)
+            val otherHand = rankHand(other + coms)
+            total += 2
+            wins += myHand.compareTo(otherHand) + 1
+        }
+        val pLose1 = 1.0 - wins / total
+        val pLose = Math.pow(pLose1, pCount - 1.0)
+
+        val pWin = 1.0 - pLose
+
+        val rBound = dikkePot*pWin/pLose
+        return when {
+            rBound - toCall >= state.minBet -> Raise(player, (rBound - toCall).toInt())
+            toCall == 0 -> Check(player)
+            rBound >= toCall -> Call(player, toCall)
+            else -> Fold(player)
+        }
+    }
 }
