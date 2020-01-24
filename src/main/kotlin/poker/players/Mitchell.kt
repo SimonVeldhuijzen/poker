1ce1f07e88ced1e6ac9f10c72ea27b2e9567a2cd
Mitchell Vrugteveen <mail@mtchll.com>
2020-01-24 15:00:03 +0100

    Update Mitchell.kt

--- src/main/kotlin/poker/players/Mitchell.kt
+++ src/main/kotlin/poker/players/Mitchell.kt
@@ -8,16 +8,15 @@ class Mitchell: AIPlayer {
     override var name: String = "Mitchell"
 
     override fun move(state: Board, player: Player): PlayerAction {
-        val listOfPlayers = listOf(MitCall(), MitchAllIn(), Mitcheck(), MitchFold(), Raisechell())
+        val listOfPlayers = listOf(MitCall(), MitchAllIn(), Mitcheck(), Raisechell())
         val listOfCards = player.cards + state.communityCards
         val pokerhand = rankHand(listOfCards)
 
         val playerAction: PlayerAction
-        println("type $pokerhand.")
         if (pokerhand is HighCard) {
-            playerAction = listOfPlayers.random().move(state, player)
-        } else {
             playerAction = Check(player)
+        } else {
+            playerAction = listOfPlayers.random().move(state, player)
         }
         return playerAction
     }
