ee244287ffd6d6ddc5f55ee8c173d97a3f7a4f97
Joeri Landman <joeri.landman@nedap.com>
2020-01-24 17:20:06 +0100

    compiles now

--- src/main/kotlin/poker/players/PokermonMaster.kt
+++ src/main/kotlin/poker/players/PokermonMaster.kt
@@ -2,21 +2,20 @@ package poker.players
 
 import poker.*
 
-class PokermonMaster(commands: String): AIPlayer {
+class PokermonMaster(): AIPlayer {
     override var name = "PokermonMaster"
-    private val commands = commands.split(" ").toMutableList()
 
     override fun move(state: Board, player: Player): PlayerAction {
         val handToRank = player.cards
         handToRank.addAll(state.communityCards)
         val rankedHand = rankHand(handToRank)
         if (rankedHand.handRank > 2) {
-            Call(player)
+            return Call(player)
         } else {
             if (player.betThisRound <= 100) {
-                Call(player)
+                return Call(player)
             } else {
-                Check(player)
+                return Check(player)
             }
         }
     }
