b7e630a2331236f54afdcb0000785e21b7d45f7d
Daan Te Bokkel <daan.tebokkel@nedap.com>
2020-01-24 11:17:36 +0100

    hm

--- src/main/kotlin/poker/players/BarryDePokerTovenaar.kt
+++ src/main/kotlin/poker/players/BarryDePokerTovenaar.kt
@@ -6,8 +6,37 @@ import poker.*
  * Created by daan.tebokkel on 1/24/20
  */
 class BarryDePokerTovenaar: AIPlayer {
+
     override fun move(state: Board, player: Player): PlayerAction {
-        return Check(player)
+        val action = checkHandBeforeFlop(player)
+        println("actie: $action")
+        if (action == "check") {
+            return Check(player)
+        } else if (action == "allin"){
+            return AllIn(player, 10000000)
+        } else if (action == "call")  {
+            return Call(player, 10)
+        }else if (action == "raise")  {
+            return Raise(player, state.currentBet * 2)
+        }else {
+            return Fold(player)
+        }
+    }
+
+
+    //check hand
+    fun checkHandBeforeFlop(player: Player): String {
+        val pokerHand = rankHand(player.cards)
+        println("Pokerhand: $pokerHand")
+        when(pokerHand) {
+            is Pair -> return "raise"
+            is HighCard -> return "allin"
+            is ThreeOfAKind -> return "raise"
+            is Flush -> return "raise"
+            is FourOfAKind -> return ""
+            is StraightFlush -> return ""
+            else -> return "fold"
+        }
     }
 
     //check board state
