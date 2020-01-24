0b3aee0267c2ff6f3f2eec8d1b445ae4da434ee0
Mitchell Vrugteveen <mail@mtchll.com>
2020-01-24 13:55:34 +0100

    pusssh

--- src/main/kotlin/poker/ScoreLogic.kt
+++ src/main/kotlin/poker/ScoreLogic.kt
@@ -23,6 +23,7 @@ fun applyShowDown(board: Board): List<kotlin.Pair<Player, PokerHand>> {
 }
 
 fun rankHand(cards: List<Card>): PokerHand {
+    println(cards.size)
     val sortedByRanks = cards.sortedByDescending { it.rank.rank }
     val straight = straight(sortedByRanks)
     if (straight != null) {
